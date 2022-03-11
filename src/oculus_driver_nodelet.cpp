08086852910cefc1f13057b77504988eb7e49552
Aaron Marburg <amarburg@uw.edu>
2022-03-11 22:22:15 +0000

    Use snake case for ip_address and frame_id parameters.

--- src/oculus_driver_nodelet.cpp
+++ src/oculus_driver_nodelet.cpp
@@ -48,9 +48,9 @@ void OculusDriver::onInit() {
   //     use the private namespace, but I'm not sure how to do that when
   //     nodelets have been compiled into the executable, rather than
   //     configured in a launch file.
-  n_.param<std::string>("ipAddress", ip_address_, "auto");
+  n_.param<std::string>("ip_address", ip_address_, "auto");
 
-  n_.param<std::string>("frameId", frame_id_, "oculus");
+  n_.param<std::string>("frame_id", frame_id_, "oculus");
   NODELET_INFO_STREAM("Publishing data with frame = " << frame_id_);
 
   data_rx_.setRawPublisher(raw_data_pub_);
